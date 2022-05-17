49861cc7645c8fcb16a3173ac47b2e1c8f556c71
pelsinkaplanfinagotech <pelsinkaplan@finagotech.com.tr>
2022-05-17 22:40:42 +0300

    Shortest PAth Algorithm methods filled.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Example
-        Node node = new Node(4);
-        node.appendToEnd(5);
-        node.appendToEnd(6);
-        node.appendToEnd(7);
+        Node node = new Node(3, 7);
+        node.data = 0.0;
+        node.appendToEnd(1, 10);
+        node.appendToEnd(1, 4);
+        node.appendToEnd(2, 1);
+        node.appendToEnd(5, 2);
+        node.appendToEnd(6, 5);
+        node.appendToEnd(8, 4);
+        node.appendToEnd(8, 9);
+        node.appendToEnd(9, 2);
+        System.out.println(node.printNodes());
         // End of Example
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,16 +1,19 @@
 public class Node {
 
     Node following = null;
+    double data;
+    double x;
+    double y;
 
-    int data;
-
-    public Node(int data) {
-        this.data = data;
+    public Node(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public void appendToEnd(int data) {
-        Node end = new Node(data);
-        Node n = this;
+    public void appendToEnd(int x, int y) {
+        Node end = new Node(x, y); // added node
+        Node n = this; // head node
+        end.data = Math.sqrt(Math.pow((n.x - x), 2) + Math.pow((n.y - y), 2));
 
         while (n.following != null) {
             n = n.following;
@@ -20,17 +23,39 @@ public class Node {
 
     // TODO:: Implement to return the length of the SinglyLinkedList
     // For example:: --> 5 --> 6 --> 7 --> 3 --> .
-    public void printNodes() {
+    public String printNodes() {
+        StringBuilder shortestPath = new StringBuilder("Factory(3, 7)");
+        Node head = this;
+
+        Node tempNode = head.following;
+        while (tempNode != null) {
+            String stringPath = String.format("%.5f", tempNode.data);
+            shortestPath.append("\nCustomer(").append(tempNode.x).append(", ").append(tempNode.y).append(") -> ").append(stringPath).append(")");
+            tempNode = tempNode.following;
+        }
+        return shortestPath.toString() + "\n" + sumOfNodes();
     }
 
     // TODO:: Implement to return the length of the SinglyLinkedList
-    int length(Node h) {
-        return 0;
+    int length() {
+        int length = 0;
+        Node tempNode = this;
+        while (tempNode != null) {
+            length++;
+            tempNode = tempNode.following;
+        }
+        return length;
     }
 
     // TODO:: Implement to return the sum of the Nodes
-    int sumOfNodes() {
-        return 0;
+    double sumOfNodes() {
+        double sumOfNodes = 0.0;
+        Node tempNode = this;
+        while (tempNode != null) {
+            sumOfNodes += tempNode.data;
+            tempNode = tempNode.following;
+        }
+        return sumOfNodes;
     }
 
     Node deleteNode(Node head, int data) {
